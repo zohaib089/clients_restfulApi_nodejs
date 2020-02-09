521b0e613c50035a38688d8bcad7b047e9b99bbe
zohaib089 <zabi.jarral08@gmail.com>
2020-02-09 19:02:23 +0100

    Client Model has been created

---
+++ Model/Client.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+const ClientSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    memborship:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    data:{
+        type:Date,
+        default:Date.now
+    }
+})
